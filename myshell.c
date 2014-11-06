dc66194eb6a77cddffcfbba184ea8ba8b1dc1eb1
stmfunk <renegadethunder@gmail.com>
2014-11-06 16:02:04 +0000

    Fixed stuff

--- myshell.c
+++ myshell.c
@@ -2,60 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
-#include <time.h>
 
 
 #define PS1 "MyShell> "
 
 // Heap Variables
 char *cwd;
-char *glob_env;
-
 
 // Function declarations
 void sigIntEventHandle(int some); // This function handles interrupts by 
                                   //returning caller to the start of the main event loop
-char *getEnv(char *envName, char *env[]); // Basic search for enviroment variables (linear)
-                                          // not very sophisticated just searches for anything matching the first part of the envName
-int ioLoop(char *env); // Main event loop takes care of IO and commands
-void graceExit(); // This handles a SIGSEGV to prevent a segmenation fault on C-d
+int ioLoop(); // Main event loop takes care of IO and commands
+//void graceExit(); // This handles a SIGSEGV to prevent a segmenation fault on C-d
 void charStrip(); // Removes trailing newlines
 
 
-int main(int argc, char *argv[], char *env[]) {
-  glob_env = *env;
+int main(int argc, char *argv[]) {
   signal(SIGINT, sigIntEventHandle);
-  signal(SIGSEGV, graceExit);
+ // signal(SIGSEGV, graceExit);
 
-  return ioLoop(&env);
+  return ioLoop();
 }
 
 
 void sigIntEventHandle(int event) {
   printf("\n");
-  ioLoop(glob_env);
+  ioLoop();
 }
 
 
-void graceExit(int event) {
-  printf("\n");
+/*void graceExit(int event) {
+  write("\n");
   exit(0);
-}
+}*/
 
 
-char *getEnv(char *envName, char *env[]) {
-  int i;
-  for (i = 0; env[i] != NULL; i++) 
-    if (strcmp(env[i], envName) > 0) 
-      break;
-  return env[i];
-}
-
-
-int ioLoop(char *env) {
+int ioLoop() {
   char *input = malloc(sizeof(char)*200);
   if (cwd == NULL)
-    cwd = getEnv("HOME", env);
+    cwd = getenv("HOME");
 
   while (1) {
     printf(PS1);
@@ -64,11 +49,11 @@ int ioLoop(char *env) {
     if (!strcmp(input,"exit")) return 0;
 
     if (strcmp(input,"cd") == 0) {
-      cwd = getEnv("HOME",env);
+      cwd = getenv("HOME");
       printf("%s\n", cwd);
     } else if (strcmp(input,"cd") > 0);
 
-    if (strcmp(input,"cwd") == 0)
+    if (strcmp(input,"pwd") == 0)
       printf("%s\n", cwd);
   }
   return 0;

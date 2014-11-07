e64c1fd1d8fa0d0bf45c0dc28675c8c9d8745804
Donal O'Shea <renegadethunder@gmail.com>
2014-11-07 13:35:28 +0000

    Modified program to use getcwd and chdir

--- myshell.c
+++ myshell.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <signal.h>
 
 
 #define PS1 "MyShell> "
 
+
 // Heap Variables
 char *cwd;
+
 
 // Function declarations
 void sigIntEventHandle(int some); // This function handles interrupts by 
                                   //returning caller to the start of the main event loop
 int ioLoop(); // Main event loop takes care of IO and commands
-//void graceExit(); // This handles a SIGSEGV to prevent a segmenation fault on C-d
+void graceExit(); // This handles a SIGSEGV to prevent a segmenation fault on C-d
 void charStrip(); // Removes trailing newlines
 
 
+
+// Struct delclarations
+
+struct args {
+  int argc;
+  char *argv;
+};
+
+
 int main(int argc, char *argv[]) {
+  cwd = malloc(sizeof(char)*150);
   signal(SIGINT, sigIntEventHandle);
- // signal(SIGSEGV, graceExit);
+  signal(SIGHUP, graceExit);
 
   return ioLoop();
 }
@@ -34,31 +47,43 @@ void sigIntEventHandle(int event) {
 }
 
 
-/*void graceExit(int event) {
-  write("\n");
+void graceExit(int event) {
+  printf("\n");
+  fflush(stdout);
   exit(0);
-}*/
+}
 
 
 int ioLoop() {
   char *input = malloc(sizeof(char)*200);
-  if (cwd == NULL)
-    cwd = getenv("HOME");
 
   while (1) {
     printf(PS1);
     fgets(input, 100, stdin);
     charStrip(input);
-    if (!strcmp(input,"exit")) return 0;
+
+    // This handles a call to exit
+    if (!strcmp(input,"exit")) {
+      int n = 0;
+      //obtainArgs(input);
+      exit(n);
+    }
 
     if (strcmp(input,"cd") == 0) {
       cwd = getenv("HOME");
+      chdir(cwd);
       printf("%s\n", cwd);
-    } else if (strcmp(input,"cd") > 0);
+    } else if (strcmp(input,"cd") > 0)
+      ;
 
-    if (strcmp(input,"pwd") == 0)
+    if (strcmp(input,"pwd") == 0) {
+      getcwd(cwd, 150);
       printf("%s\n", cwd);
+    }
+
+    *input = '\0';
   }
+  free(input);
   return 0;
 }
 
